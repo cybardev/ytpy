0a51ed952f316613b7b65d062a06a03fa2a25429
Sheikh <sheikh@cybar.dev>
2022-10-23 22:47:39 -0300

    Update ytpy.py

--- ytpy.py
+++ ytpy.py
@@ -41,9 +41,7 @@ def error(err_code=0, msg=None, **kwargs):
     @param msg: the error message
     @param **kwargs: extra messages
     """
-    # if no error message given...
     if msg == None:
-        # set the error message to usage info
         msg = str(
             "Usage: ytpy [OPTIONS] <search query>\n"
             + "           OPTIONS:\n"
@@ -55,13 +53,11 @@ def error(err_code=0, msg=None, **kwargs):
             + "\n"
             + "List of mpv hotkeys: https://defkey.com/mpv-media-player-shortcuts"
         )
-    # print the given or default error message
+
     print(msg)
-    # if any extra messages are given...
     for err, err_msg in kwargs.items():
-        # print the extra messages
         print(f"{err}: {err_msg}")
-    # exit with given or default error code
+
     sys.exit(err_code)
 
 
@@ -71,29 +67,22 @@ def check_deps(deps_list):
 
     @param deps_list: list of dependencies to check
     """
-    # check each item in given dependency list
-    # and see if it is installed on the system
     for deps in deps_list:
-        # if it is not installed
         if not installed(deps):
-            # show an error message and exit with code 1
             error(1, msg=f"Dependency {deps} not found.\nPlease install it.")
 
 
 def filter_dupes(id_list):
     """
     Generator to filter out duplicates from a list of strings
+    Used instead of set() to preserve order of search results
 
     @param li: the list to be filtered
     """
-    seen = set()  # set of seen IDs
-    # for each video ID in list li...
+    seen = set()
     for video_id in id_list:
-        # if it's unique...
         if video_id not in seen:
-            # add it to the list of seen IDs
             seen.add(video_id)
-            # and return it to the caller
             yield video_id
 
 
@@ -104,10 +93,9 @@ def get_media_url(search_str="rickroll"):
     @param search_str: the string to search for
     @return the deduced media URL
     """
-    # compile regex pattern for faster search
     video_id_re = re.compile(r'"videoId":"(.{11})"')
-    # format the given search string for use in URLs
     query_string = parse.urlencode({"search_query": search_str})
+
     # when connected to the internet...
     try:
         # get the YouTube search-result page for given search string
@@ -118,49 +106,39 @@ def get_media_url(search_str="rickroll"):
         )
     # if not connected to the internet...
     except urlerr.URLError:
-        # raise an error and quit
         error(1, "No internet connection.")
-    # find the list of video IDs from result page
+
     search_results = list(filter_dupes(video_id_re.findall(html_content)))
-    # if no results are found...
+
     if len(search_results) == 0:
-        # print error message and exit
         error(msg="No results found.")
-    # select the nth result...
+
     video_id = search_results[RESULT_NUM - 1]
-    # and deduce its URL
     media_url = "https://www.youtube.com/watch?v=" + video_id
-    # return the URL of requested media
+
     return media_url
 
 
-def play(search_str, options):
+def play(media_url, options):
     """
     Call the media player and play requested media
 
     @param options: the command line arguments to the player
     @param search_str: the string to search for
     """
-    # check if dependencies are satisfied
-    check_deps([PLAYER, DOWNLOADER])
     # if everything is ok, play requested media
-    os.system(f"{PLAYER} {options} {get_media_url(search_str)}")
+    os.system(f"{PLAYER} {options} {media_url}")
 
 
-def download(search_str):
+def download(media_url):
     """
     Call the media downloader and download requested media
 
     @param search_str: the string to search for
     """
-    # check if dependencies are satisfied
-    check_deps(
-        [DOWNLOADER, "ffmpeg"]
-    )  # ffmpeg may be needed to merge downloaded media
-    # if everything is ok, download requested media
     os.system(
         f"{DOWNLOADER} -o '{DLOAD_DIR}%(title)s.%(ext)s' \
-                {get_media_url(search_str)}"
+                {media_url}"
     )
 
 
@@ -173,55 +151,44 @@ def sentinel_prompt(ans, sym="λ"):
     @param sym the symbol to show in the prompt (purely decorative)
     @return string of query words
     """
-    # while no answer is given...
     while len(ans) == 0:
-        # keep nagging user for input
         print("Please enter search query:")
         ans = " ".join(input(f"❮{sym}❯ ").split()).strip()
-    # return the entered words as a string
+
     return ans
 
 
 def argparse():
-    # parse flags and arguments
+    """
+    Process flags and arguments
+
+    @return search query and mpv flags
+    """
+    req_search: str = ""
+    flags: str = ""
+
+    # TODO: use proper conditional logic instead of relying on exceptions
     try:
-        # decide whether to play video or audio only for the session
         opts, extras = getopt.getopt(sys.argv[1:], "hudv:")
 
-        # if options contain help flag...
         if "-h" in opts[0]:
-            # show help and exit normally
             error()
-        # if options contain URL flag...
         if "-u" in opts[0]:
-            # show the URL and quit
             error(0, get_media_url(opts[0][1] + " ".join(extras).rstrip()))
-        # if options contain download flag...
         elif "-d" in opts[0]:
-            # process the name of media to search
-            req_search = opts[0][1] + " ".join(extras).rstrip()
-            # download the requested media
-            download(req_search)
-            # exit normally
+            check_deps([DOWNLOADER, "ffmpeg"])
+            download(get_media_url(opts[0][1] + " ".join(extras).rstrip()))
             sys.exit(0)
-        # if options contain video flag...
         elif "-v" in opts[0]:
-            # process the name of media to search
             req_search = opts[0][1] + " ".join(extras).rstrip()
-            # set flags to empty, to use defaults
             flags = ""
-    # if invalid flags are used...
     except getopt.GetoptError:
         error(2, UnknownArgs="Unknown options given.")
-    # when no flags are given...
     except IndexError:
-        # and no arguments are given...
         if OP_MODE == "music":
-            # and default operation mode is set to music...
             prompt_sym = "🎵"
             flags = "--ytdl-format=bestaudio --no-video"
         elif OP_MODE == "video":
-            # and default operation mode is set to music...
             prompt_sym = "🎬"
             flags = ""
         else:
@@ -230,44 +197,31 @@ def argparse():
                 UnknownValue="variable OP_MODE has an unknown value."
                 + '\nValid options are "music" and "video"',
             )
-        # when arguments are given, prepare to play media
+
         req_search = sentinel_prompt(extras, prompt_sym)
 
+    check_deps([PLAYER])  # TODO: check at the moment it's decided to play media
     return req_search, flags
 
 
 def loop(query, flags):
-    # play the requested item and loop over input
+    cache_url:str = ""
     while query not in ("", "q"):
-        # call the mpv media player with processed flags and URL
-        play(query, flags)
-        # when done, ask if user wants to repeat the last played media
+        media_url = cache_url if cache_url else get_media_url(query)
+        play(media_url, flags)
         answer = input("Play again? (y/n): ")
-        # process user request
-        if answer.lower() in {"n", ""}:
-            # if user answers no, ask what to play next, or quit
+        if answer.lower() != "y":
             query = input("Play next (q to quit): ")
-        elif answer.lower() == "y":
-            # if user answers yes, keep playing
-            continue
         else:
-            # if invalid option is chosen, exit with code 2
-            error(2, "Unrecognized option. Quitting...")
+            cache_url = media_url
 
 
-# when invoked as a program...
 if __name__ == "__main__":
-    # execute the main program logic
-    # and process flags and arguments
     """
     Main program logic
     """
-    # while the user doesn't quit by pressing ^C (Ctrl+C)...
     try:
         loop(*argparse())
-    # if user presses ^C (Ctrl+C) to quit the program
     except KeyboardInterrupt:
-        # show a message and quit
         pass
-    # exit normally when everything is done
     error(0, "\nQuitting...")
